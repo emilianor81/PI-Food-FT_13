17a5cf402dc99171e09ed3ee8af8cb339a3d0224
emilianor81 <emilianor81@gmail.com>
2022-06-13 23:53:58 -0300

    Fix css styles

--- client/src/components/recipe/Recipe.js
+++ client/src/components/recipe/Recipe.js
@@ -7,30 +7,19 @@ import image from '../../img/default.jpg'
 
 function Recipe(props) {
   return(
-      <div className='Recipe' >
-           <div>
+        <div className='Recipe'>
              <h3>{props.title} </h3> 
              <h5>Score : {props.score}</h5>
              <div className='Diets'>
-             {props.diets.filter(d => {if (props.diet !== undefined){return props.diet.includes(d.name.toLowerCase()) || props.diet.find(diet => d.name === diet)}
-      else{return props.diets}})
-        .map(d => <span className='diets' key={d.id}>{d.name?d.name:props.diet}</span>)}
-        
-                  {/* {props.diets.filter(d => 
-                        {return props.diet.includes(d.name.toLowerCase()) || props.diet.find(diets => d.name === diets.name)}
-                        )
-                    .map(d => <span className='diets' key={d.id}>{d.name}</span>)}    */}
-                   
-              </div>
+                    {props.diets.filter(d => {if (props.diet !== undefined){return props.diet.includes(d.name.toLowerCase()) || props.diet.find(diet => d.name === diet)}
+              else{return props.diets}})
+                .map(d => <span className='diets' key={d.id}>{d.name?d.name:props.diet}</span>)}          
+             </div>
               <Link to="/detail"><button className='buton' onClick={() => props.searchRecipeDetail(props.id)}>Detail</button></Link> 
              <img src={props.img?props.img: image} className='RecipeImage' alt='recipe' />         
-
-           </div>
-
-      </div>
+        </div> 
   )
 }
-
 
 function mapStateToProps(state) {
   return {
