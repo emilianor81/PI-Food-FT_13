a584dfac4f9876b8086edacf337f530a2542ff10
emilianor81 <emilianor81@gmail.com>
2022-07-13 09:37:10 -0300

    delete unuses code

--- api/src/routes/diet.js
+++ api/src/routes/diet.js
@@ -1,17 +1,9 @@
 const {Router} = require('express');
-const {BASE_URL, URL_COMPLEX, URL_DETAIL } = require('../../constants');
-const API_KEY = process.env.API_KEY;
 const router = Router();
-const axios = require('axios');
-const { v4: uuid } = require('uuid');
-const express = require('express');
-// const Diet = require('../models/Diet');
 const { Diet } = require('../db')
 
 
 const dietTypes = router.get('/',async (req, res) =>{
-  // HACER UN GET DE TODAS LAS RECETAS Y SACARLE A CADA UNA LAS DIETAS Y ESTAS DIETAS PONERLAS EN UN ARREGLO
-  // Y METERLA EN MI TABLA DIETA SI NO EXISTEN
        try {
         const dietsBD = await Diet.findAll()
         return res.json(dietsBD)
